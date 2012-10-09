1aee2e18ab2fd366594aeae0154693f876cbc4c1
Steven Klise <sklise@gmail.com>
2012-10-09 00:18:33 -0400

    Comment, clean, refactor

--- server.js
+++ server.js
@@ -5,16 +5,15 @@ var app = express.createServer(express.logger());
 // require and create a socket.io instance
 var io = require('socket.io').listen(app);
 
+// Setup static file serving from the public folder.
 app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
 
-app.get('/', function(request, response) {
-  response.sendfile(__dirname + '/public/index.html')
-})
-
+// Try to get the port from the environment, otherwise set it to 5000.
 var port = process.env.PORT || 5000;
 
+// Start the app
 app.listen(port, function() {
   console.log('Listening on ' + port);
 });
@@ -22,13 +21,15 @@ app.listen(port, function() {
 // Set up socket listeners when a connection is opened.
 io.sockets.on('connection', function(socket) {
 
-  // To emit a function, you must also call the function. Maybe don't do this.
-  socket.emit('welcome', function() {
-    return {welcomeAt: Date.now()}
-  }());
+  // Emit a socket message after connection, call it a "welcome" event.
+  socket.emit('welcome', {welcomeAt: Date.now()});
 
+  // Broadcast that a new user has joined. This emit will go to all clients
+  // except the originator.
   socket.broadcast.emit('join', {welcome: 'new user'});
 
+  // Listen for a 'mouseClicked' message from the client, and broadcast upon
+  // receipt.
   socket.on('mouseClicked', function(data) {
     socket.broadcast.emit('addedCell', {x: data.x, y: data.y});
   });
